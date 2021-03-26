dadccaafb6e3380c4df2cd290baa7e8b6b03119b
Viraj Shah <virajtshah1999@gmail.com>
2021-03-26 20:10:41 +0530

    Add files via upload

--- Q1.php
+++ Q1.php
@@ -13,13 +13,12 @@
     
     $t2 = (time() + 1000);
     echo($t2 . "<br>");
-    $at1=$_POST['submit'];
+    $at1="<script>document.write(formInitializationTime)</script>";
     $str="INSERT INTO answer "."(name,a1,at1) "."VALUES ('$name','$a1','$at1')";
     $result=mysqli_query($con,$str);
     header("location:Q2.php");
   }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -29,7 +28,6 @@
       $time= time();
       $atime = date('h:i:s A',$time);
       echo $atime;}
-
       function et(){
         date_default_timezone_set('Asia/Kolkata');
         $time1= time()+10;
@@ -38,7 +36,6 @@
         $at1= $atime1-$atime;
         echo($at1);}*/
       ?>
-
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
@@ -49,23 +46,16 @@
     <style type="text/css">
       html, body { width: 100%; height: 100%; overflow: hidden; background-color:rgba(55, 0, 255, 0.397);}
     </style>
-    <script>
-      var formInitializationTime;
-      $('form input').bind('keypress change click', function () {
-          if (!formInitializationTime) formInitializationTime = new Date();
-      });
-
-      $('form').bind('submit', function () {
-          document.querySelector('input.duration').value = new Date() - formInitializationTime;
-      });
-      <?php 
-      echo "Hello";
-      echo("<script>document.write(at1);</script>");
-      ?>
+    <script type="text/javascript">
+      var formInitializationTime = new Date();
+      function submitDuration() {
+	      formInitializationTime = new Date() - formInitializationTime;
+	      console.log(formInitializationTime);
+	    }
+      document.write(new Date().getTime() / 1000);
     </script>
   </head>
   <body >
-    <1?php stt();?>
     <div class="header">
       <h3>Dyslexia Data Colletion</h3>
       <h3>Capstone Project At Vellore Institute of Technology</h3>
@@ -85,13 +75,12 @@
                 <input type="radio" name="a1" value="Male" class="form-control:focus" required />Male 
               </div>
               <div id="home" class="flex-center flex-column">
-                <button class="btn btn-primary btn-block" id="submit" name="submit">Next</button>
+                <button class="btn btn-primary btn-block" id="submit" name="submit" onclick=submitDuration()>Next</button>
               </div>
             </form>
           </div>
         </div>
 			</div>
 		</section>
-    <1?php et(); ?>
   </body>
 </html>
